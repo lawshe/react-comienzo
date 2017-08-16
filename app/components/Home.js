b74a1d95f58936f24eece7854851c6df30db8986
lawshe <jessica@jlawshe.com>
2017-08-16 16:07:23 -0500

    Remove nonexistent class

--- app/components/Home.js
+++ app/components/Home.js
@@ -28,7 +28,7 @@ class Home extends React.Component {
     return (
       <div>
         <Intro parallax={this.props.parallax} />
-        <div className={`section ${glob.no_pad} green darken-4 ${glob.pattern}`}>
+        <div className={`section ${glob.no_pad} green darken-4`}>
           <div className={`row ${glob.no_margin}`}>
             <div className={`col s12 light-green darken-4 ${glob.pad_lg}`}>
               <h4 className="grey-text text-lighten-4" style={{ marginTop: '0' }}>Subheader</h4>
